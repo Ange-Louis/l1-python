0d5c9c54e6486619739280c235743b5c30d2b909
Ange-Louis SAMMARCELLI <ange-louis.sammarcelli@ens.uvsq.fr>
2021-11-30 17:14:47 +0100

    TD05

--- exercises/TD05_gui/dessin.py
+++ exercises/TD05_gui/dessin.py
@@ -12,9 +12,16 @@ import tkinter as tk
 racine = tk.Tk() # Création de la fenêtre racine
 label = tk.Label(racine, text= "Mon Dessin", font = ("algerian", "30"), bg = "black", fg = "white") # création d'un widget
 button1 = tk.Button(text= "Choisir une couleur", font = ("gigi", "20")) # création d'un button
+button2 = 
+button3 = 
+button4 = 
 canvas = tk.Canvas(racine, width = 500, height = 500, bg = "black")
-label.grid(column=0, row=0, padx= "0cm") # positionnement du premier widget
-button1.grid(column=1, row=1, pady= "0cm") # positionnement du premier widget
-label.grid()
-button1.grid()
+
+# Fin du code 
+label.grid(column = 1, row = 0, padx = 10)
+button1.grid(column = 1, row = 1, padx = 10, pady = 10)
+button2.grid(column = 0, row = 2, pady = 10)
+button3.grid(column = 0, row = 2, pady = 10)
+button4.grid(column = 0, row = 2, pady = 10)
+canvas.grid(column = 1, row = 2, padx = 10, pady = 10)
 racine.mainloop() # Lancement de la boucle principale
